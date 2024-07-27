f01c1cd25f1b3a311b909746ee077a390fcd14cf
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-27 09:38:47 +0000

    Add print_char and print_str functions

--- main.h
+++ main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int print_char(char c);
+int print_str(char *s);
 
 #endif /* ifndef MAIN_H */
---
+++ print_cs.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+#include <stddef.h>
+
+/**
+ * print_char - Prints a character
+ * @c: Character to be printed
+ * Return: Number of characters printed
+ */
+
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_str - Prints a string
+ * @s: String to be printed
+ * Return: Number of characters printed
+ */
+
+int print_str(char *s)
+{
+	int count;
+
+	if (!s)
+		return (0);
+	for (count = 0; s[count] != '\0'; count++)
+		print_char(s[count]);
+	return (count);
+}
