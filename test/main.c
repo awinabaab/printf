40b69b16702f09e51cb4457a1538a6bd5c9464a0
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-28 12:17:06 +0000

    Update

--- test/main.c
+++ test/main.c
@@ -9,20 +9,31 @@ int main(void)
 	int num = -123;
 	char *str = "Hello, world!";
 	char ch = 'A';
+	int count;
 /*	unsigned int u_num = 456; */
 /*  unsigned int hex_num = 0xdeadbeef; */
 /*	unsigned int hex_upper = 0xDEAFBEEF; */
 
-	_printf("%d\n", num);
-	_printf("%i\n", num);
-	_printf("%c\n", ch);
-	_printf("%s\n", str);
-	_printf("%%\n");
-	_printf("%s\n", "Hello World!");
-	_printf("%c\n", 'c');
-	_printf("Hello!\n");
-	_printf("%\n");
-	_printf("Hi %d, how %i, wh %c, hn %s, ij %%\n", num, num, ch, str);
+	count = _printf("%d\n", num);
+	_printf("%d\n", count);
+	count = _printf("%i\n", num);
+	_printf("%d\n", count);
+	count = _printf("%c\n", ch);
+	_printf("%d\n", count);
+	count = _printf("%s\n", str);
+	_printf("%d\n", count);
+	count = _printf("%%\n");
+	_printf("%d\n", count);
+	count = _printf("%s\n", "Hello World!");
+	_printf("%d\n", count);
+	count = _printf("%c\n", 'c');
+	_printf("%d\n", count);
+	count = _printf("Hello!\n");
+	_printf("%d\n", count);
+	count = _printf("%\n");
+	_printf("%d\n", count);
+	count = _printf("Hi %d, how %i, wh %c, hn %s, ij %%\n", num, num, ch, str);
+	_printf("%d\n", count);
 	
 /*	_printf("Integer: %d\nUnsigned: %u\nString: %s\nChar: %c\nHex lower: %x\nHex upper: %X\n", num, str, ch); */
 	return (0);
