c925f2d1580f19085e65ee06d7b5019d6f2d53e3
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-28 16:35:31 +0000

    Add str_functions.c

---
+++ str_functions.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+#include <stddef.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * print_char - Prints a character
+ * @c: Character to be printed
+ * Return: Number of characters printed
+ */
+
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_str - Prints a string
+ * @s: String to be printed
+ * Return: Number of characters printed
+ */
+
+int print_str(char *s)
+{
+	int count;
+
+	count = 0;
+	if (!s)
+		return (count);
+	for (count = 0; s[count] != '\0'; count++)
+		print_char(s[count]);
+	return (count);
+}
