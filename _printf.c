2d8d3925c4a1fb19d3dcbebc831d672bbdf1d716
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-28 10:42:07 +0000

    Add _printf.c

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - Formats and prints data
+ * @format: Foramt to be printed
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			format_parser(format, args);
+		}
+		else
+			print_char(*format);
+		format++;
+	}
+	va_end(args);
+
+	return (0);
+}
