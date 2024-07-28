bf67e0c56c0b0a85dc6b39940b531197b178d3f5
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-28 10:41:05 +0000

    Add format_parser function prototype

--- main.h
+++ main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int print_char(char c);
-int print_str(char *s);
+int print_str(const char *s);
 int print_deci(int n);
 int print_psign(void);
+void format_parser(const char *format, va_list args);
 
 #endif /* ifndef MAIN_H */
