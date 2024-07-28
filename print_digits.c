86145a0680e742ac5b95b8b6aa5572cd0946a8e1
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-28 12:16:05 +0000

    Modify character count logic

--- print_digits.c
+++ print_digits.c
@@ -11,16 +11,29 @@ int print_deci(int n)
 	unsigned int num = n;
 	int count;
 
+	count = 0;
 	if (n < 0)
 	{
 		print_char('-');
 		num = -num;
-	}
-	if (num / 10)
-	{
-		count = print_deci(num / 10);
-	}
-	count += print_char((num % 10) + '0');
+		if (num / 10)
+		{
+			count = print_deci(num / 10);
+		}
+		count += print_char((num % 10) + '0');
+		count++;
 
+		return (count);
+	}
+	if (n >= 0)
+	{
+		if (num / 10)
+		{
+			count = print_deci(num / 10);
+		}
+		count += print_char((num % 10) + '0');
+
+		return (count);
+	}
 	return (count);
 }
