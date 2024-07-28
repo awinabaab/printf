0b14f528cabe18709b09928319fc6045aba07c44
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-28 12:14:13 +0000

    Return number of characters printed

--- format_parser.c
+++ format_parser.c
@@ -8,32 +8,34 @@
  * Return: void
  */
 
-void format_parser(const char *format, va_list args)
+int format_parser(const char *format, va_list args)
 {
 	char ch;
 	char *str;
 	int num;
+	int count;
 
 	switch (*format)
 	{
 		case '%':
-			print_char('%');
+			count = print_char('%');
 			break;
 		case 'c':
 			ch = va_arg(args, int);
-			print_char(ch);
+			count = print_char(ch);
 			break;
 		case 's':
 			str = va_arg(args, char *);
-			print_str(str);
+			count = print_str(str);
 			break;
 		case 'd':
 			num = va_arg(args, int);
-			print_deci(num);
+			count = print_deci(num);
 			break;
 		case 'i':
 			num = va_arg(args, int);
-			print_deci(num);
+			count = print_deci(num);
 			break;
 	}
+	return (count);
 }
