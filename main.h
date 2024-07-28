28972b5f8727ca95547c8e52d52c2bd377488786
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-28 16:18:29 +0000

    Update main.h prototypes

--- main.h
+++ main.h
@@ -4,9 +4,8 @@
 
 int _printf(const char *format, ...);
 int print_char(char c);
-int print_str(const char *s);
+int print_str(char *s);
 int print_deci(int n);
-int print_psign(void);
 int format_parser(const char *format, va_list args);
 
 #endif /* ifndef MAIN_H */
