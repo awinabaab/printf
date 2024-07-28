740d9fb27d9810076e3439f6baf5813f513c39e5
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-28 21:25:13 +0000

    Update format_parser function prototype

--- main.h
+++ main.h
@@ -6,6 +6,6 @@ int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *s);
 int print_int(int n);
-int format_parser(const char *format, va_list args);
+int format_parser(const char *format, va_list *args);
 
 #endif /* ifndef MAIN_H */
