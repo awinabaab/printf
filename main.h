10b9dc47df220be85ed31531fa624557377f1aef
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-28 16:33:55 +0000

    Update function prototype

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *s);
-int print_deci(int n);
+int print_int(int n);
 int format_parser(const char *format, va_list args);
 
 #endif /* ifndef MAIN_H */
