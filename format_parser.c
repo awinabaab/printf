a1dee64665c088dbf1ca62202405fffd10770b81
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-28 10:41:38 +0000

    Add format_parser function

---
+++ format_parser.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * format_parser - Parses a format string and executes the appropriate function
+ * @format: Format to be parsed
+ * @args: Optional arguments
+ * Return: void
+ */
+
+void format_parser(const char *format, va_list args)
+{
+	char ch;
+	char *str;
+	int num;
+	unsigned int number;
+
+	switch (*format)
+	{
+		case '%':
+			print_char('%');
+			break;
+		case 'c':
+			ch = va_arg(args, int);
+			print_char(ch);
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			print_str(str);
+			break;
+		case 'd':
+			num = va_arg(args, int);
+			print_deci(num);
+			break;
+		case 'i':
+			num = va_arg(args, int);
+			print_deci(num);
+			break;
+		default:
+			print_str("Format not found\n");
+			break;
+	}
+}
