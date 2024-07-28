f15b048f4e59c9b23bb9658d82f4b2ffc704f950
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-28 22:36:02 +0000

    Modify header file and num data type

--- num_functions.c
+++ num_functions.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include <unistd.h>
-#include <stddef.h>
 
 /**
  * print_int - Convert an int to a string and write it to standard output
- * @num: Number to be converted and printed
+ * @n: Number to be converted and printed
  *
  * Return: Number of characters printed
  */
 
 int print_int(int n)
 {
-	int num = n;
+	unsigned int num = n;
 	int count;
 
 	count = 0;
-	if (num < 0)
+	if (n < 0)
 	{
 		count += print_char('-');
 		num = -num;
