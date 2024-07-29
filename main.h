d41e4aee2ea2d802ea7a271c604d7e5c467350bf
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-29 11:03:41 +0000

    Add print_num_base and print_hex_upper function prototypes

--- main.h
+++ main.h
@@ -6,6 +6,8 @@ int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *s);
 int print_int(int n);
+int print_num_base(unsigned int, int);
+int print_hex_upper(unsigned int);
 int format_parser(const char *format, va_list *args);
 
 #endif /* ifndef MAIN_H */
