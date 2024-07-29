f925ede98ec10e62151d1189d68fb1a545dd798e
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-07-29 15:08:02 +0000

    Add print_u and _power function protoypes

--- main.h
+++ main.h
@@ -6,9 +6,11 @@ int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *s);
 int print_rev(char *s);
-int print_int(int n);
+int print_int(long int n);
 int print_num_base(unsigned int, int);
 int print_hex_upper(unsigned int);
+long int _power(double base, int expo);
+int print_u(int num);
 int format_parser(const char *format, va_list *args);
 
 #endif /* ifndef MAIN_H */
