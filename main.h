02e8c70c995b2656555f4a5f8b4238062f6725c3
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-29 11:53:26 +0000

    Add print_rev function prototype

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *s);
+int print_rev(char *s);
 int print_int(int n);
 int print_num_base(unsigned int, int);
 int print_hex_upper(unsigned int);
