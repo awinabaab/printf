5d8dc43d3b1499df42f3fd8e1ed80c928e4ea450
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-30 15:53:32 +0000

    Update

--- test/task6.c
+++ test/task6.c
@@ -5,7 +5,12 @@ int main(void)
 {
 	void *addr;
 	int a = 28;
-	int *ptr = &a;
+	char c = '*';
+	float f = 0.0;
+	double d = 0.0;
+	char ch = ';';
+	char *s = &ch;
+	char **str = &s;
 	int count;
 
     addr = (void *)0x7ffe637541f0;
@@ -13,8 +18,19 @@ int main(void)
    _printf("%d\n", count);
  	count = printf("Address:[%p]\n", addr);
 	printf("%d\n", count);
-    _printf("Addre:[%p]\n", ptr);
- 	printf("Addre:[%p]\n", ptr);
+    _printf("Addre:[%p]\n", &a);
+ 	printf("Addre:[%p]\n", &a);
+    _printf("Addre:[%p]\n", &c);
+ 	printf("Addre:[%p]\n", &c);
+    _printf("Addre:[%p]\n", &f);
+ 	printf("Addre:[%p]\n", &f);
+    _printf("Addre:[%p]\n", &d);
+ 	printf("Addre:[%p]\n", &d);
+    _printf("Addre:[%p]\n", &ch);
+ 	printf("Addre:[%p]\n", &ch);
+    _printf("Addre:[%p]\n", *str);
+ 	printf("Addre:[%p]\n", *str);
+
 
 	return (0);
 }
