0edb91e8226837b058f40f76cac40e668e5151e2
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-30 13:41:31 +0000

    Add print_pointer function prototype

--- main.h
+++ main.h
@@ -6,11 +6,12 @@ int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *s);
 int print_int(long int n);
-int print_num_base(unsigned int, int);
+int print_num_base(unsigned long, int);
 int print_hex_upper(unsigned int);
 long int _power(double base, int expo);
 int print_u(int num);
 int non_print_chars(char *str);
+int print_pointer(void *);
 int format_parser(const char *format, va_list *args);
 
 #endif /* ifndef MAIN_H */
