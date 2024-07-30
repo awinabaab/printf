3dc81625c0cadc5c9fa5f8484496bf7c289b7da9
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-30 14:55:28 +0000

    Add ptr_hex_convert function

---
+++ pointer_function.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_pointer - Prints an address
+ * @addr: Pointer to be printed
+ *
+ * Return: Number of characters printed
+ */
+
+int print_pointer(void *addr)
+{
+	unsigned long ptr = (unsigned long)addr;
+	int count = 0;
+
+	count += print_str("0x");
+	count += ptr_hex_convert(ptr, 16);
+
+	return (count);
+}
+
+/**
+ * ptr_hex_convert - Converts a long int to hex and prints it to stdout
+ * @num: Number to be printed
+ * @base: Number base to be printed in
+ *
+ * Return: Number of characters printed
+ */
+
+int ptr_hex_convert(unsigned long num, int base)
+{
+	char buf[BUF_SIZE];
+	int index = BUF_SIZE - 1;
+	const char hex_chars[] = "0123456789abcdef";
+
+	buf[index--] = '\0';
+
+	do {
+		buf[index--] = hex_chars[num % base];
+		num /= base;
+	} while (num > 0);
+
+	return (print_str(&buf[index + 1]));
+}
