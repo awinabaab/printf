64b39c26a54894e914c303a84bbe60ef8945784e
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-31 11:35:57 +0000

    Add length modifier tests

--- test/test_main.c
+++ test/test_main.c
@@ -14,6 +14,19 @@ int main(void)
     unsigned int ui;
     void *addr;
     int count;
+	long int ld = 127000000000509;
+	long int li = 127457452984509;
+	long int lo = 98;
+	long int lu = 300;
+	long int lhl = 11;
+	long int lhu = 11;
+/*	short int sd = 300;
+	short int si = 300;
+*/
+	short int so = 98;
+	short int su = 300;
+	short int shl = 11;
+	short int shu = 11;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
@@ -43,6 +56,65 @@ int main(void)
 
    count =  _printf("Unknown: [%R]\n[%R]\n", "ABYcdrDbfgcd", "123");
    printf("%d\n", count);
+
+   _printf("CUSTOM printf");
+   _printf("LONG\n");
+   count =  _printf("Long int: [%ld]\n", ld);
+   printf("%d\n", count);
+   count =  _printf("Long int: [%li]\n", li);
+   printf("%d\n", count);
+   count =  _printf("Long octal: [%lo]\n", lo);
+   printf("%d\n", count);
+   count =  _printf("Long unsigned: [%lu]\n", lu);
+   printf("%d\n", count);
+   count =  _printf("Long Hex lower: [%lx]\n", lhl);
+   printf("%d\n", count);
+   count =  _printf("Long Hex upper: [%lX]\n", lhu);
+   printf("%d\n", count);
+   _printf("\n\n\n");
+   _printf("SHORT\n");
+   count =  _printf("Short int: [%hd]\n", 12700000000509);
+   printf("%d\n", count);
+   count =  _printf("Short int: [%hi]\n", 127457452984509);
+   printf("%d\n", count);
+   count =  _printf("Short octal: [%ho]\n", so);
+   printf("%d\n", count);
+   count =  _printf("Short unsigned: [%hu]\n", su);
+   printf("%d\n", count);
+   count =  _printf("Short Hex lower: [%hx]\n", shl);
+   printf("%d\n", count);
+   count =  _printf("Short Hex upper: [%hX]\n", shu);
+   printf("%d\n", count);
+   _printf("\nREGULAR printf");
+   printf("LONG\n");
+   count =  printf("Long int: [%ld]\n", ld);
+   printf("%d\n", count);
+   count =  printf("Long int: [%li]\n", li);
+   printf("%d\n", count);
+   count =  printf("Long octal: [%lo]\n", lo);
+   printf("%d\n", count);
+   count =  printf("Long unsigned: [%lu]\n", lu);
+   printf("%d\n", count);
+   count =  printf("Long Hex lower: [%lx]\n", lhl);
+   printf("%d\n", count);
+   count =  printf("Long Hex upper: [%lX]\n", lhu);
+   printf("%d\n", count);
+   _printf("\n\n\n");
+   _printf("SHORT\n");
+   count =  printf("Short int: [%hd]\n", 12700000000509);
+   printf("%d\n", count);
+   count =  printf("Short int: [%hi]\n", 127457452984509);
+   printf("%d\n", count);
+   count =  printf("Short octal: [%ho]\n", so);
+   printf("%d\n", count);
+   count =  printf("Short octal: [%ho]\n", so);
+   count = printf("Short unsigned: [%hu]\n", su);
+   printf("%d\n", count);
+   count =  printf("Short Hex lower: [%hx]\n", shl);
+   printf("%d\n", count);
+   count =  printf("Short Hex upper: [%hX]\n", shu);
+   printf("%d\n", count);
+
 
     return (0);
 }
